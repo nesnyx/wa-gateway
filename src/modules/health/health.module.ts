a29c4bbbe3d5d783db1fdaf46ef3ad67fede2e75
arisbara <baraaris2@gmail.com>
2026-03-25 16:37:31 +0800

    refactor: Rearrange imports and add HealthController to HealthModule

--- src/modules/health/health.module.ts
+++ src/modules/health/health.module.ts
@@ -1,8 +1,10 @@
-
 import { Module } from '@nestjs/common';
-import { TerminusModule } from '@nestjs/terminus';
 
+import { HealthController } from './health.controller';
+import { TerminusModule } from '@nestjs/terminus';
 @Module({
-  imports: [TerminusModule]
+  imports: [TerminusModule],
+  controllers: [HealthController],
+
 })
-export class HealthModule {}
+export class HealthModule { }
