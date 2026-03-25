a5adf5e30fe8d6d7a02e418de13af622f8b84c08
arisbara <baraaris2@gmail.com>
2026-03-25 14:43:06 +0800

    feat: Update session management logic, enhance error handling, and improve reconnection strategy for WhatsApp service

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 import { TransformInterceptor } from './core/interceptors/transformer.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: ['http://localhost:5173', 'https://wa.nadinata.org'],
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      allowedHeaders: 'Content-Type, Accept, Authorization',
-
-    }
-  });
+  const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   app.useGlobalInterceptors(new TransformInterceptor());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
-  await app.listen(3343);
+  await app.listen(6667);
 }
 bootstrap();
